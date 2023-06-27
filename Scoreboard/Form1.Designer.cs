2350ded044334b785d6bbc9b73ad21bb4d48296a
B Meidus <benediktas,meidus@gmail.com>
2023-06-27 16:47:22 +0100

    Completed a lot of buttons and dat

--- Scoreboard/Form1.Designer.cs
+++ Scoreboard/Form1.Designer.cs
@@ -44,24 +44,38 @@
             AwayPlus3 = new Button();
             button1 = new Button();
             label1 = new Label();
+            button2 = new Button();
+            button3 = new Button();
+            TimeoutTimer = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            HomeFouls = new Label();
+            AwayFouls = new Label();
+            HomeTimeouts = new Label();
+            AwayTimeouts = new Label();
+            HomeFoul = new Button();
+            AwayFoul = new Button();
+            HomeTimeout = new Button();
+            AwayTimeout = new Button();
+            TimeoutTick = new System.Windows.Forms.Timer(components);
             SuspendLayout();
             // 
             // TimerDisplay
             // 
             TimerDisplay.AutoSize = true;
             TimerDisplay.ForeColor = Color.Red;
-            TimerDisplay.Location = new Point(277, 69);
+            TimerDisplay.Location = new Point(242, 52);
             TimerDisplay.Name = "TimerDisplay";
-            TimerDisplay.Size = new Size(47, 20);
+            TimerDisplay.Size = new Size(37, 15);
             TimerDisplay.TabIndex = 0;
             TimerDisplay.Text = "Timer";
             // 
             // StartTimer
             // 
-            StartTimer.Location = new Point(326, 88);
-            StartTimer.Margin = new Padding(3, 4, 3, 4);
+            StartTimer.Location = new Point(285, 66);
             StartTimer.Name = "StartTimer";
-            StartTimer.Size = new Size(86, 31);
+            StartTimer.Size = new Size(75, 23);
             StartTimer.TabIndex = 1;
             StartTimer.Text = "Start/Stop";
             StartTimer.UseVisualStyleBackColor = true;
@@ -75,18 +89,17 @@
             // 
             PosessionTimer.AutoSize = true;
             PosessionTimer.ForeColor = Color.Goldenrod;
-            PosessionTimer.Location = new Point(510, 69);
+            PosessionTimer.Location = new Point(446, 52);
             PosessionTimer.Name = "PosessionTimer";
-            PosessionTimer.Size = new Size(118, 20);
+            PosessionTimer.Size = new Size(95, 15);
             PosessionTimer.TabIndex = 2;
             PosessionTimer.Text = " Posession Timer";
             // 
             // ResetPosession
             // 
-            ResetPosession.Location = new Point(418, 88);
-            ResetPosession.Margin = new Padding(3, 4, 3, 4);
+            ResetPosession.Location = new Point(366, 66);
             ResetPosession.Name = "ResetPosession";
-            ResetPosession.Size = new Size(125, 31);
+            ResetPosession.Size = new Size(109, 23);
             ResetPosession.TabIndex = 3;
             ResetPosession.Text = "Reset Possession";
             ResetPosession.UseVisualStyleBackColor = true;
@@ -95,27 +108,26 @@
             // HomeScore
             // 
             HomeScore.AutoSize = true;
-            HomeScore.Location = new Point(304, 199);
+            HomeScore.Location = new Point(266, 123);
             HomeScore.Name = "HomeScore";
-            HomeScore.Size = new Size(17, 20);
+            HomeScore.Size = new Size(13, 15);
             HomeScore.TabIndex = 4;
             HomeScore.Text = "0";
             // 
             // AwayScore
             // 
             AwayScore.AutoSize = true;
-            AwayScore.Location = new Point(546, 199);
+            AwayScore.Location = new Point(477, 123);
             AwayScore.Name = "AwayScore";
-            AwayScore.Size = new Size(17, 20);
+            AwayScore.Size = new Size(13, 15);
             AwayScore.TabIndex = 5;
             AwayScore.Text = "0";
             // 
             // HomePlus1
             // 
-            HomePlus1.Location = new Point(273, 235);
-            HomePlus1.Margin = new Padding(3, 4, 3, 4);
+            HomePlus1.Location = new Point(239, 176);
             HomePlus1.Name = "HomePlus1";
-            HomePlus1.Size = new Size(86, 31);
+            HomePlus1.Size = new Size(75, 23);
             HomePlus1.TabIndex = 6;
             HomePlus1.Text = "+1";
             HomePlus1.UseVisualStyleBackColor = true;
@@ -123,10 +135,9 @@
             // 
             // HomePlus2
             // 
-            HomePlus2.Location = new Point(273, 273);
-            HomePlus2.Margin = new Padding(3, 4, 3, 4);
+            HomePlus2.Location = new Point(239, 205);
             HomePlus2.Name = "HomePlus2";
-            HomePlus2.Size = new Size(86, 31);
+            HomePlus2.Size = new Size(75, 23);
             HomePlus2.TabIndex = 7;
             HomePlus2.Text = "+2";
             HomePlus2.UseVisualStyleBackColor = true;
@@ -134,10 +145,9 @@
             // 
             // HomePlus3
             // 
-            HomePlus3.Location = new Point(273, 312);
-            HomePlus3.Margin = new Padding(3, 4, 3, 4);
+            HomePlus3.Location = new Point(239, 234);
             HomePlus3.Name = "HomePlus3";
-            HomePlus3.Size = new Size(86, 31);
+            HomePlus3.Size = new Size(75, 23);
             HomePlus3.TabIndex = 8;
             HomePlus3.Text = "+3";
             HomePlus3.UseVisualStyleBackColor = true;
@@ -145,10 +155,9 @@
             // 
             // AwayPlus1
             // 
-            AwayPlus1.Location = new Point(510, 235);
-            AwayPlus1.Margin = new Padding(3, 4, 3, 4);
+            AwayPlus1.Location = new Point(446, 176);
             AwayPlus1.Name = "AwayPlus1";
-            AwayPlus1.Size = new Size(86, 31);
+            AwayPlus1.Size = new Size(75, 23);
             AwayPlus1.TabIndex = 9;
             AwayPlus1.Text = "+1";
             AwayPlus1.UseVisualStyleBackColor = true;
@@ -156,10 +165,9 @@
             // 
             // AwayPlus2
             // 
-            AwayPlus2.Location = new Point(510, 273);
-            AwayPlus2.Margin = new Padding(3, 4, 3, 4);
+            AwayPlus2.Location = new Point(446, 205);
             AwayPlus2.Name = "AwayPlus2";
-            AwayPlus2.Size = new Size(86, 31);
+            AwayPlus2.Size = new Size(75, 23);
             AwayPlus2.TabIndex = 10;
             AwayPlus2.Text = "+2";
             AwayPlus2.UseVisualStyleBackColor = true;
@@ -167,10 +175,9 @@
             // 
             // AwayPlus3
             // 
-            AwayPlus3.Location = new Point(510, 312);
-            AwayPlus3.Margin = new Padding(3, 4, 3, 4);
+            AwayPlus3.Location = new Point(446, 234);
             AwayPlus3.Name = "AwayPlus3";
-            AwayPlus3.Size = new Size(86, 31);
+            AwayPlus3.Size = new Size(75, 23);
             AwayPlus3.TabIndex = 11;
             AwayPlus3.Text = "+3";
             AwayPlus3.UseVisualStyleBackColor = true;
@@ -178,9 +185,10 @@
             // 
             // button1
             // 
-            button1.Location = new Point(677, 151);
+            button1.Location = new Point(592, 37);
+            button1.Margin = new Padding(3, 2, 3, 2);
             button1.Name = "button1";
-            button1.Size = new Size(106, 29);
+            button1.Size = new Size(93, 22);
             button1.TabIndex = 12;
             button1.Text = "New period";
             button1.UseVisualStyleBackColor = true;
@@ -189,18 +197,169 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(704, 128);
+            label1.Location = new Point(592, 20);
             label1.Name = "label1";
-            label1.Size = new Size(66, 20);
+            label1.Size = new Size(53, 15);
             label1.TabIndex = 13;
             label1.Text = "Period: 1";
             // 
+            // button2
+            // 
+            button2.Location = new Point(366, 95);
+            button2.Name = "button2";
+            button2.Size = new Size(134, 23);
+            button2.TabIndex = 14;
+            button2.Text = "Reset Possession to 14";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(340, 281);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 15;
+            button3.Text = "Withdraw";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // TimeoutTimer
+            // 
+            TimeoutTimer.AutoSize = true;
+            TimeoutTimer.Location = new Point(328, 9);
+            TimeoutTimer.Name = "TimeoutTimer";
+            TimeoutTimer.Size = new Size(19, 15);
+            TimeoutTimer.TabIndex = 16;
+            TimeoutTimer.Text = "00";
+            TimeoutTimer.Visible = false;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(188, 123);
+            label3.Name = "label3";
+            label3.Size = new Size(40, 15);
+            label3.TabIndex = 17;
+            label3.Text = "Points";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(190, 138);
+            label4.Name = "label4";
+            label4.Size = new Size(35, 15);
+            label4.TabIndex = 18;
+            label4.Text = "Fouls";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(188, 153);
+            label5.Name = "label5";
+            label5.Size = new Size(56, 15);
+            label5.TabIndex = 19;
+            label5.Text = "Timeouts";
+            // 
+            // HomeFouls
+            // 
+            HomeFouls.AutoSize = true;
+            HomeFouls.Location = new Point(266, 138);
+            HomeFouls.Name = "HomeFouls";
+            HomeFouls.Size = new Size(13, 15);
+            HomeFouls.TabIndex = 20;
+            HomeFouls.Text = "0";
+            // 
+            // AwayFouls
+            // 
+            AwayFouls.AutoSize = true;
+            AwayFouls.Location = new Point(477, 138);
+            AwayFouls.Name = "AwayFouls";
+            AwayFouls.Size = new Size(13, 15);
+            AwayFouls.TabIndex = 21;
+            AwayFouls.Text = "0";
+            // 
+            // HomeTimeouts
+            // 
+            HomeTimeouts.AutoSize = true;
+            HomeTimeouts.Location = new Point(266, 153);
+            HomeTimeouts.Name = "HomeTimeouts";
+            HomeTimeouts.Size = new Size(13, 15);
+            HomeTimeouts.TabIndex = 22;
+            HomeTimeouts.Text = "5";
+            // 
+            // AwayTimeouts
+            // 
+            AwayTimeouts.AutoSize = true;
+            AwayTimeouts.Location = new Point(477, 153);
+            AwayTimeouts.Name = "AwayTimeouts";
+            AwayTimeouts.Size = new Size(13, 15);
+            AwayTimeouts.TabIndex = 23;
+            AwayTimeouts.Text = "5";
+            // 
+            // HomeFoul
+            // 
+            HomeFoul.Location = new Point(158, 176);
+            HomeFoul.Name = "HomeFoul";
+            HomeFoul.Size = new Size(75, 23);
+            HomeFoul.TabIndex = 24;
+            HomeFoul.Text = "Foul";
+            HomeFoul.UseVisualStyleBackColor = true;
+            HomeFoul.Click += HomeFoul_Click;
+            // 
+            // AwayFoul
+            // 
+            AwayFoul.Location = new Point(527, 176);
+            AwayFoul.Name = "AwayFoul";
+            AwayFoul.Size = new Size(75, 23);
+            AwayFoul.TabIndex = 25;
+            AwayFoul.Text = "Foul";
+            AwayFoul.UseVisualStyleBackColor = true;
+            AwayFoul.Click += AwayFoul_Click;
+            // 
+            // HomeTimeout
+            // 
+            HomeTimeout.Location = new Point(158, 205);
+            HomeTimeout.Name = "HomeTimeout";
+            HomeTimeout.Size = new Size(75, 23);
+            HomeTimeout.TabIndex = 26;
+            HomeTimeout.Text = "Timeout";
+            HomeTimeout.UseVisualStyleBackColor = true;
+            HomeTimeout.Click += HomeTimeout_Click;
+            // 
+            // AwayTimeout
+            // 
+            AwayTimeout.Location = new Point(527, 205);
+            AwayTimeout.Name = "AwayTimeout";
+            AwayTimeout.Size = new Size(75, 23);
+            AwayTimeout.TabIndex = 27;
+            AwayTimeout.Text = "Timeout";
+            AwayTimeout.UseVisualStyleBackColor = true;
+            AwayTimeout.Click += AwayTimeout_Click;
+            // 
+            // TimeoutTick
+            // 
+            TimeoutTick.Tick += TimeoutTick_Tick;
+            // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = Color.FromArgb(74, 64, 99);
-            ClientSize = new Size(800, 450);
+            BackColor = Color.White;
+            ClientSize = new Size(700, 338);
+            Controls.Add(AwayTimeout);
+            Controls.Add(HomeTimeout);
+            Controls.Add(AwayFoul);
+            Controls.Add(HomeFoul);
+            Controls.Add(AwayTimeouts);
+            Controls.Add(HomeTimeouts);
+            Controls.Add(AwayFouls);
+            Controls.Add(HomeFouls);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(TimeoutTimer);
+            Controls.Add(button3);
+            Controls.Add(button2);
             Controls.Add(label1);
             Controls.Add(button1);
             Controls.Add(AwayPlus3);
@@ -216,7 +375,6 @@
             Controls.Add(StartTimer);
             Controls.Add(TimerDisplay);
             FormBorderStyle = FormBorderStyle.Fixed3D;
-            Margin = new Padding(3, 4, 3, 4);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
@@ -240,5 +398,20 @@
         private Button AwayPlus3;
         private Button button1;
         private Label label1;
+        private Button button2;
+        private Button button3;
+        private Label TimeoutTimer;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label HomeFouls;
+        private Label AwayFouls;
+        private Label HomeTimeouts;
+        private Label AwayTimeouts;
+        private Button HomeFoul;
+        private Button AwayFoul;
+        private Button HomeTimeout;
+        private Button AwayTimeout;
+        private System.Windows.Forms.Timer TimeoutTick;
     }
 }
--- Scoreboard/Form1.cs
+++ Scoreboard/Form1.cs
@@ -6,6 +6,9 @@ namespace Scoreboard
     {
         Stopwatch sw = new Stopwatch();
         Stopwatch psw = new Stopwatch();
+        Stopwatch timeoutSW = new Stopwatch();
+        int withdraw = 1;
+        int currentPosLength = 24;
         int period = 1;
         public Form1()
         {
@@ -32,20 +35,22 @@ namespace Scoreboard
         {
             long time = 600000 - sw.ElapsedMilliseconds;
             TimerDisplay.Text = $"{time / 60000:00}:{(time / 1000) - 60 * (time / 60000):00}";
-            if (psw.ElapsedMilliseconds / 1000 == 24)
+            if (psw.ElapsedMilliseconds / 1000 == currentPosLength)
             {
                 timer1.Stop();
                 sw.Stop();
                 psw.Stop();
                 psw.Reset();
+                currentPosLength = 24;
             }
-            PosessionTimer.Text = $"{(24 - psw.ElapsedMilliseconds / 1000.0):00.0}";
+            PosessionTimer.Text = $"{(currentPosLength - psw.ElapsedMilliseconds / 1000.0):00.0}";
         }
 
         private void ResetPosession_Click(object sender, EventArgs e)
         {
             psw.Reset();
-            PosessionTimer.Text = $"{(24 - psw.ElapsedMilliseconds / 1000.0):00.0}";
+            currentPosLength = 24;
+            PosessionTimer.Text = $"{(currentPosLength - psw.ElapsedMilliseconds / 1000.0):00.0}";
             if (timer1.Enabled)
             {
                 psw.Start();
@@ -55,32 +60,32 @@ namespace Scoreboard
 
         private void HomePlus1_Click(object sender, EventArgs e)
         {
-            HomeScore.Text = (Convert.ToInt16(HomeScore.Text) + 1).ToString();
+            HomeScore.Text = (Convert.ToInt16(HomeScore.Text) + withdraw * 1).ToString();
         }
 
         private void HomePlus2_Click(object sender, EventArgs e)
         {
-            HomeScore.Text = (Convert.ToInt16(HomeScore.Text) + 2).ToString();
+            HomeScore.Text = (Convert.ToInt16(HomeScore.Text) + withdraw * 2).ToString();
         }
 
         private void HomePlus3_Click(object sender, EventArgs e)
         {
-            HomeScore.Text = (Convert.ToInt16(HomeScore.Text) + 3).ToString();
+            HomeScore.Text = (Convert.ToInt16(HomeScore.Text) + withdraw * 3).ToString();
         }
 
         private void AwayPlus1_Click(object sender, EventArgs e)
         {
-            AwayScore.Text = (Convert.ToInt16(AwayScore.Text) + 1).ToString();
+            AwayScore.Text = (Convert.ToInt16(AwayScore.Text) + withdraw * 1).ToString();
         }
 
         private void AwayPlus2_Click(object sender, EventArgs e)
         {
-            AwayScore.Text = (Convert.ToInt16(AwayScore.Text) + 2).ToString();
+            AwayScore.Text = (Convert.ToInt16(AwayScore.Text) + withdraw * 2).ToString();
         }
 
         private void AwayPlus3_Click(object sender, EventArgs e)
         {
-            AwayScore.Text = (Convert.ToInt16(AwayScore.Text) + 3).ToString();
+            AwayScore.Text = (Convert.ToInt16(AwayScore.Text) + withdraw * 3).ToString();
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -94,7 +99,7 @@ namespace Scoreboard
                 psw.Stop(); psw.Reset();
                 long time = 600000 - sw.ElapsedMilliseconds;
                 TimerDisplay.Text = $"{time / 60000:00}:{(time / 1000) - 60 * (time / 60000):00}";
-                PosessionTimer.Text = $"{(24 - psw.ElapsedMilliseconds / 1000.0):00.0}";
+                PosessionTimer.Text = $"{(currentPosLength - psw.ElapsedMilliseconds / 1000.0):00.0}";
             }
         }
         protected override void OnFormClosing(FormClosingEventArgs e)
@@ -103,6 +108,75 @@ namespace Scoreboard
             {
                 e.Cancel = true;
             }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            psw.Reset();
+            currentPosLength = 14;
+            PosessionTimer.Text = $"{(currentPosLength - psw.ElapsedMilliseconds / 1000.0):00.0}";
+            if (timer1.Enabled)
+            {
+                psw.Start();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            withdraw *= -1;
+        }
+
+        private void AwayFoul_Click(object sender, EventArgs e)
+        {
+            AwayFouls.Text = Convert.ToString(Convert.ToInt16(AwayFouls.Text) + 1);
+            sw.Stop();
+            psw.Stop(); psw.Reset();
+            PosessionTimer.Text = $"{(currentPosLength - psw.ElapsedMilliseconds / 1000.0):00.0}";
+        }
+
+        private void HomeFoul_Click(object sender, EventArgs e)
+        {
+            HomeFouls.Text = Convert.ToString(Convert.ToInt16(HomeFouls.Text) + 1);
+            sw.Stop();
+            psw.Stop(); psw.Reset();
+            PosessionTimer.Text = $"{(currentPosLength - psw.ElapsedMilliseconds / 1000.0):00.0}";
+        }
+
+        private void HomeTimeout_Click(object sender, EventArgs e)
+        {
+            sw.Stop();
+            psw.Stop();
+            HomeTimeouts.Text = Convert.ToString(Convert.ToInt16(HomeTimeouts.Text) - 1);
+            TimeoutTimer.Visible = true;
+            timeoutSW.Start();
+            TimeoutTick.Start();
+            TimeoutTimer.Text = $"{timeoutSW.ElapsedMilliseconds:00}";
+        }
+
+        private void TimeoutTick_Tick(object sender, EventArgs e)
+        {
+            if (60000 - timeoutSW.ElapsedMilliseconds < 0)
+            {
+                TimeoutTimer.Visible = false;
+                timeoutSW.Stop();
+                TimeoutTick.Stop();
+            }
+            else
+            {
+                TimeoutTimer.Text = $"{(60000 - timeoutSW.ElapsedMilliseconds) / 1000:00}";
+            }
+
+        }
+
+        private void AwayTimeout_Click(object sender, EventArgs e)
+        {
+            sw.Stop();
+            psw.Stop();
+            AwayTimeouts.Text = Convert.ToString(Convert.ToInt16(AwayTimeouts.Text) - 1);
+            TimeoutTimer.Visible = true;
+            timeoutSW.Start();
+            TimeoutTick.Start();
+            TimeoutTimer.Text = $"{timeoutSW.ElapsedMilliseconds:00}";
         }
     }
 }
